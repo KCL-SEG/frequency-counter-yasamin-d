d05f38a942b48a8df34e3455a5cc191dcbefd040
yasamin-d <yasamindivsalar@gmail.com>
2022-12-08 19:28:04 +0000

    Implement frequency counter

--- frequencies.py
+++ frequencies.py
@@ -1,12 +1,22 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
+from collections import Counter
 
 def frequencies(items):
     frequency = {}
-
+    stringItems = []
     for item in items:
-        if str(item) in frequency:
-            frequency[str(item)] += 1
+        stringItems.append(str(item))
+
+    for item in stringItems:
+        if item not in frequency:
+            frequency[item] = 1
         else:
-            frequency[str(item)] = 1
+            frequency[item] += 1
+    # word_count= Counter(stringItems)
+    # frequency = dict(word_count)
+    #     if str(item) in frequency:
+    #         frequency[str(item)] += 1
+    #     else:
+    #         frequency[str(item)] = 1
     print(frequency)
